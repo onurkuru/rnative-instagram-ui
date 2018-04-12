8f1b8642800d3524f50dfb542ee1e61082a58c1c
Onur Kuru <onur@Onurs-MacBook-Air.local>
2018-04-12 22:45:30 +0300

    searchtab

--- Components/AppTabNavigator/SearchTab.js
+++ Components/AppTabNavigator/SearchTab.js
@@ -2,11 +2,22 @@ import React, {Component}  from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Dimensions,
+  ScrollView
 } from 'react-native';
 
-import { Icon } from 'native-base'
+import { Container, Content, Icon, Item, Input, Thumbnail, Header, Body, Left, Right, Button } from 'native-base'
 
+ var images = [
+    require('../../assets/feeds/1.jpg'),
+    require('../../assets/feeds/2.jpg'),
+    require('../../assets/feeds/3.jpg'),
+    require('../../assets/feeds/4.jpg')
+  ]
+
+  var {width,height} = Dimensions.get('window')
 
 class SearchTab extends Component {
 
@@ -18,23 +29,90 @@ static navigationOptions = {
 
   )
 }
+ renderSectionOne = () => {
+
+    return images.map((image, index) => {
+      return (
+       <View key={index} style={[{width: (width)/3}, {height: (width)/3}, {marginBottom: 2}, index % 3 !== 0 ? {paddingLeft: 2} : {paddingLeft: 0}  ]} >
+         <Image style={{flex:1, width: undefined, height: undefined}}
+              source={image} />
+       </View>
+       )
+    })
+
+  } 
 
 	render(){
 		return (
-			<View style={styles.container}>
-				<Text>SearchTab</Text>
-        </View>
-		);
-	}
+		<Container style={{flex: 1, backgroundColor: 'white'}}>
+        <Header>
+          <Item>
+           <Input placeholder="Search" />
+          </Item>
+        </Header>
+
+       <Content>
+
+          <View style={{height: 100}} >
+       
+            <View style={{flex: 3}}>
+              <ScrollView 
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                  contentContainerStyle={{
+                    alignItems:  'center',
+                    paddingStart: 5,
+                    paddingEnd: 5 
+                  }}
+                >
+                 <Thumbnail 
+                    style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
+                    source={require
+                        ('../../assets/stories/1.jpg')} />
+                  <Thumbnail 
+                    style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
+                    source={require
+                        ('../../assets/stories/2.jpg')} />
+                  <Thumbnail 
+                    style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
+                    source={require
+                        ('../../assets/stories/3.jpg')} />
+                  <Thumbnail 
+                    style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
+                    source={require
+                        ('../../assets/stories/4.jpg')} />
+                  <Thumbnail 
+                    style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
+                    source={require
+                        ('../../assets/stories/5.jpg')} />
+                  <Thumbnail 
+                    style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
+                    source={require
+                        ('../../assets/stories/6.jpg')} />
+          <Thumbnail 
+                    style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
+                    source={require
+                        ('../../assets/stories/7.jpg')} />              </ScrollView>
+            </View>
+
+
+          </View>
+            <View style={{flexDirection: 'row', flexWrap: 'wrap'  }}>
+           {this.renderSectionOne()}
+          </View>
+
+              </Content>
+      </Container>
+			
+	);
+  }
 }
 export default SearchTab;
 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
- 
+  flex: 1,
+  backgroundColor: 'white'
+  }
 });
